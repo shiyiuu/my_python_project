6c18425faf50284be6693b1d31104ef6e0d8b155
shiyiuu <cyb_shiyi@163.com>
2024-07-06 17:54:14 +0800

    方法_check_events()

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -21,10 +21,7 @@ class AlienInvasion:
     def run_game(self):
         '''开始游戏主循环'''
         while True:
-            # 监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
             
             # 每次循环时都重绘屏幕。
             self.screen.fill(self.settings.bg_color)
@@ -32,6 +29,13 @@ class AlienInvasion:
             
             # 让最近绘制的屏幕可见
             pygame.display.flip()
+    
+    def _check_events(self):
+        '''响应键盘和鼠标事件'''
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
