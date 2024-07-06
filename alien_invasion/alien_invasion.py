ab908e1e0212b8a7900ec53503d4a40ab4d4e3fd
shiyiuu <cyb_shiyi@163.com>
2024-07-06 18:25:32 +0800

    update 驾驶飞船

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -13,6 +13,9 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
 
@@ -22,14 +25,34 @@ class AlienInvasion:
         '''开始游戏主循环'''
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
     
     def _check_events(self):
-        '''响应键盘和鼠标事件'''
-
+        """响应鼠标和按键事件。"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """响应按键。"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """响应松开。"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
     
     def _update_screen(self):
           """更新屏幕上的图像，并切换到新屏幕。"""
