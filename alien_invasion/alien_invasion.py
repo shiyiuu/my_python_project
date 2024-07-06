093cca1f48289aa733ac2c18bbd3dab810b568bc
shiyiuu <cyb_shiyi@163.com>
2024-07-06 20:46:08 +0800

    update

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     '''管理游戏资源和行为的类'''
@@ -26,6 +27,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
     
     def _check_events(self):
@@ -46,6 +48,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+              self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """响应松开。"""
@@ -54,10 +58,17 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
     
+    def _fire_bullet(self):
+        """创建一颗子弹，并将其加入到子弹列表中。"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
           """更新屏幕上的图像，并切换到新屏幕。"""
           self.screen.fill(self.settings.bg_color)
           self.ship.blitme()
+          for bullet in self.bullets.sprites():
+              bullet.draw_bullet()
 
           pygame.display.flip()
 
