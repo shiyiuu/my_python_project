0ed96929ebf91649a20d61ea92701152002a452e
shiyiuu <cyb_shiyi@163.com>
2024-07-06 17:48:52 +0800

    在屏幕上绘制飞船

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     '''管理游戏资源和行为的类'''
@@ -14,6 +15,8 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
+
+        self.ship = Ship(self)
     
     def run_game(self):
         '''开始游戏主循环'''
@@ -25,6 +28,7 @@ class AlienInvasion:
             
             # 每次循环时都重绘屏幕。
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             
             # 让最近绘制的屏幕可见
             pygame.display.flip()
