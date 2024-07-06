57aabd41db833ded8bf78f2861d9b43189c38e11
shiyiuu <cyb_shiyi@163.com>
2024-07-06 22:06:31 +0800

    update 武装飞船

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -10,25 +10,25 @@ class AlienInvasion:
     '''管理游戏资源和行为的类'''
 
     def __init__(self):
-        '''初始化游戏并创建游戏资源'''
+        """初始化游戏并创建游戏资源"""
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
-
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
     
     def run_game(self):
         '''开始游戏主循环'''
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            self._update_screen()
+            self._update_bullets()
+            self._update_screen()       
     
     def _check_events(self):
         """响应鼠标和按键事件。"""
@@ -49,7 +49,7 @@ class AlienInvasion:
         elif event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_SPACE:
-              self._fire_bullet()
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """响应松开。"""
@@ -59,18 +59,28 @@ class AlienInvasion:
             self.ship.moving_left = False
     
     def _fire_bullet(self):
-        """创建一颗子弹，并将其加入到子弹列表中。"""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        """创建新子弹并将其加入编组bullets中。"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    
+    def _update_bullets(self):
+        """更新子弹的位置并删除消失的子弹。"""
+        # 更新子弹的位置。
+        self.bullets.update()
+
+        # 删除消失的子弹。
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                 self.bullets.remove(bullet)
 
     def _update_screen(self):
-          """更新屏幕上的图像，并切换到新屏幕。"""
-          self.screen.fill(self.settings.bg_color)
-          self.ship.blitme()
-          for bullet in self.bullets.sprites():
-              bullet.draw_bullet()
-
-          pygame.display.flip()
+        """更新屏幕上的图像，并切换到新屏幕。"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
